614cc0db788cbefe989cf29a5831efd4b9e76a24
Charles Ofuasia <ofu23001@byui.edu>
2024-10-16 20:52:17 +0100

    Corrections

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConnection');
+const cors = require('cors');
 const session = require('express-session');
 const Mongostore = require('connect-mongo');
 const passport = require('passport');
@@ -41,6 +42,14 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE', 'PATCH'],
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization',
+  })
+);
 app.use('/', require('./routes'));
 
 passport.use(
