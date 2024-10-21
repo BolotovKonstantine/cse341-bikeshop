c4d0c2880ad406511d9f1502b2c22ee757caa35b
Kjirsten Hernandez <sandlot7665@gmail.com>
2024-10-20 21:48:35 -0600

    Added authentication to orders routes

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -1,42 +1,49 @@
 const router = require('express').Router();
 const validation = require('../utilities/orderValidation')
 const orderController = require('../controller/orderController');
+const { isAuthenticated } = require('../utilities/authenticate');
 
 //------ Create/Delete an Order ------//
 // create new order
 router.post('/', 
+      // #swagger.tags = ['Orders']
+    isAuthenticated,
     validation,
     orderController.createOrder)
 //delete an order
 router.delete('/:order',
+    // #swagger.tags = ['Orders']
+    isAuthenticated,
     orderController.deleteOrder)
 
 //------ Look Up an Existing Order -----//
 
 router.get('/', 
+    // #swagger.tags = ['Orders']
     orderController.getAllOrders
 )
 
-// get order by order id
+// get order by orderId
 router.get('/:id', 
+    // #swagger.tags = ['Orders']
     orderController.findOrderById)
-
-// get all orders for user
-router.get('/user/:user', (req, res) => {
-    console.log('this is your orders by user route')
-})
-
 
 //------ Updating Existing Orders ------//
 //add or remove an item on an order
 router.put('/additems/:orderId', 
+    // #swagger.tags = ['Orders']
+    isAuthenticated,
     orderController.addProduct
 )
 router.put('/deleteitems/:orderId', 
+    // #swagger.tags = ['Orders']
+    isAuthenticated,
     orderController.deleteProduct
 )
 
 router.put('/changeuser/:orderId',
+    // #swagger.tags = ['Orders']
+    isAuthenticated,
     orderController.changeUser
 )
 
