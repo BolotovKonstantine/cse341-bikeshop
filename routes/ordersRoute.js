3469f8a74cc6fec7ba170dd1564f638f62955700
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-10-22 16:34:44 -0600

    added swagger descriptions and removed extraneous comments

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -4,15 +4,17 @@ const orderController = require('../controller/orderController');
 const { isAuthenticated } = require('../utilities/authenticate');
 
 //------ Create/Delete an Order ------//
-// create new order
+
 router.post('/', 
       // #swagger.tags = ['Orders']
+      // #swagger.description = 'Create a new order'
     isAuthenticated,
     validation,
     orderController.createOrder)
-//delete an order
+
 router.delete('/:order',
     // #swagger.tags = ['Orders']
+    // #swagger.description = 'Delete an order'
     isAuthenticated,
     orderController.deleteOrder)
 
@@ -20,29 +22,33 @@ router.delete('/:order',
 
 router.get('/', 
     // #swagger.tags = ['Orders']
+    // #swagger.description = 'Display all orders'
     orderController.getAllOrders
 )
 
-// get order by orderId
 router.get('/:id', 
     // #swagger.tags = ['Orders']
+    // #swagger.description = 'Display a specific order'
     orderController.findOrderById)
 
 //------ Updating Existing Orders ------//
-//add or remove an item on an order
+
 router.put('/additems/:orderId', 
     // #swagger.tags = ['Orders']
+    // #swagger.description = 'Add product/quantity to an existing order'
     isAuthenticated,
     orderController.addProduct
 )
 router.put('/deleteitems/:orderId', 
     // #swagger.tags = ['Orders']
+    // #swagger.description = 'Remove product/quantity from an existing order'
     isAuthenticated,
     orderController.deleteProduct
 )
 
 router.put('/changeuser/:orderId',
     // #swagger.tags = ['Orders']
+    // #swagger.description = 'change a user on an existing order'
     isAuthenticated,
     orderController.changeUser
 )
