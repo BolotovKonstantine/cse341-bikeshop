8c868c9d8498fc9ba50ad199762df7493d08537c
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-10-24 15:07:03 -0600

    temporary change to swagger: http to https and vice versa

--- swagger.js
+++ swagger.js
@@ -37,7 +37,7 @@ const doc = {
     description: 'Description',
   },
   host: 'cse341-bikeshop-5jff.onrender.com',
-  schemes: ['http', 'https'],
+  schemes: ['https', 'http'],
   components: {
     schemas: {
       Product: productSchemaSwagger,
