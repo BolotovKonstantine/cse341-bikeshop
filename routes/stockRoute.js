041954e4b32a5089d9b43b6b1b8c59ddfdfd9fba
Charles Ofuasia <ofu23001@byui.edu>
2024-10-25 20:59:01 +0100

    Added stocks tags

--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const stockController = require('../controller/stockController');
 
-router.post('/', stockController.addNewStock);
-router.get('/all-products', stockController.getAllStocks);
-router.get('/:product',  stockController.getStocksByProduct);
+router.post(
+  // #swagger.tags = ['Stocks']
+  '/',
+  stockController.addNewStock
+);
+router.get(
+  // #swagger.tags = ['Stocks']
+  '/all-products',
+  stockController.getAllStocks
+);
+router.get(
+  // #swagger.tags = ['Stocks']
+  '/:product',
+  stockController.getStocksByProduct
+);
 // router.put('/:stock', stockController.updateStock);
-router.delete('/:stock', stockController.deleteStockById);
+router.delete(
+  // #swagger.tags = ['Stocks']
+  '/:stock',
+  stockController.deleteStockById
+);
 
 module.exports = router;
