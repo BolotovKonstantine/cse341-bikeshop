e2a8a8df51b05b01e9e8f7cf23c89d5086c8ced8
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-10-27 21:58:59 -0600

    changes to ordervalidation

--- utilities/orderValidation.js
+++ utilities/orderValidation.js
@@ -1,58 +1,3 @@
-/* require('../model/User');
-require('../model/Product');
-
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const Product = mongoose.model('Product');
-const User = mongoose.model('User');
-
-const objectIdValidator = (value) => {
-    if (!mongoose.Types.ObjectId.isValid(value)) {
-      throw new Error(`${value} is not a valid ID type`);
-    }
-    return value;
-  };
-
-const productsSchema = Joi.object({
-    productId: Joi.string()
-      .required()
-      .custom(objectIdValidator, 'ObjectId Validation') 
-      .messages({
-        'string.base': 'Product ID should be a string',
-        'string.empty': 'Product ID is required',
-        'any.required': 'Product ID is required',
-      }),
-    quantity: Joi.number().integer().min(1).required().messages({
-      'number.base': 'Quantity should be a number',
-      'number.integer': 'Quantity must be an integer',
-      'number.min': 'Quantity must be at least 1',
-      'any.required': 'Quantity is required',
-    }),
-  });
-
-const orderSchema = Joi.object({
-    products: Joi.array().items(productsSchema).min(1).required().messages({
-      'array.base': 'Products should be an array',
-      'array.min': 'At least one product is required',
-      'any.required': 'Products field is required',
-    }),
-    userId: Joi.string()
-      .required()
-      .custom(objectIdValidator, 'ObjectId Validation')
-      .messages({
-        'string.base': 'User ID should be a string',
-        'string.empty': 'User ID is required',
-        'any.required': 'User ID is required',
-      }),
-  });
-
-
-
-module.exports = objectIdValidator;
-
-*/
-
-
 require('../model/User');
 require('../model/Product');
 
@@ -109,16 +54,3 @@ const validateOrder = async (req, res, next) => {
 }
 
 module.exports = validateOrder;
-
-
-// const validateOrder = async (req, res, next) => {
-//     console.log(req.body);
-//     try {
-//         await orderSchema.validateAsync(req.body);
-
-//         next()
-//     } catch (error) {
-//         res.status(400).json({message :`Validation error: ${error.message}`})
-//     }
-// }
-
